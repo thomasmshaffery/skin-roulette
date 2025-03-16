d135db0f93cf51aa6bac6c5a0dc6f772a35eb3ab
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-03-16 01:27:42 -0400

    API Retrieval, Gun objects created, Gun array stored

--- script.js
+++ script.js
@@ -4,12 +4,36 @@ const animatedBackground = document.getElementById("background");
  * Gun skin retrieval
  */
 
+let guns = [];
+
 async function getGuns() {
   const url = `https://valorant-api.com/v1/weapons/skins`;
+
   try {
     const response = await fetch(url);
-    console.log(response);
+
+    if (!response.ok) {
+      throw new Error("Could not retrieve data.");
+    }
+
+    const data = await response.json();
+    for (let i = 0; i < data.data.length; i++) {
+      generateGuns(data.data[i]);
+    }
   } catch (error) {
-    console.error.log(error.message);
+    console.error(error);
+  }
+  console.log(guns);
+}
+
+function generateGuns(Object) {
+  let gun = {
+    uuid: Object.uuid,
+    name: Object.displayName,
+    image: Object.displayIcon,
+  };
+
+  if (Object.displayIcon != null) {
+    guns.push(gun);
   }
 }
