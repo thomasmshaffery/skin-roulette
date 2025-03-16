64d60256df655894e1d5a75e8b20e0a5e50897fc
tomshaffery <98631104+tomshaffery@users.noreply.github.com>
2025-03-15 23:50:39 -0400

    Added API fetch

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 const animatedBackground = document.getElementById("background");
+
+/**
+ * Gun skin retrieval
+ */
+
+async function getGuns() {
+  const url = `https://valorant-api.com/v1/weapons/skins`;
+  try {
+    const response = await fetch(url);
+    console.log(response);
+  } catch (error) {
+    console.error.log(error.message);
+  }
+}
